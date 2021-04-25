d3aa53defee16406d65246297774449478ab420b
nduginets <nduginets@devexperts.com>
2021-04-25 12:50:28 +0300

    logic grouping

---
+++ brasilian-code/resize-images.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+while getopts a:s:i: option
+do
+	case "${option}"
+		in
+		a) ATTRI_DIR=${OPTARG};;
+		s) SEG_DIR=${OPTARG};;
+		i) IMAGE_DIR=${OPTARG};;
+	esac
+done
+
+echo "Attribute directory: $ATTRI_DIR"
+echo "Segmentation directory: $SEG_DIR"
+echo "Image directory $IMAGE_DIR"
+
+mkdir attribute_resized
+cd attribute_resized
+find ../"$ATTRI_DIR" -name '*.png' -exec sh -c 'echo "{}"; convert "{}" -resize 1024x512\> -size 1024x512 xc:red +swap -gravity center -composite `basename "{}" .png`.png' \;
+cd ..
+
+mkdir segmentation_resized
+cd segmentation_resized
+find ../"$SEG_DIR" -name '*.png' -exec sh -c 'echo "{}"; convert "{}" -resize 1024x512\> -size 1024x512 xc:red +swap -gravity center -composite `basename "{}" .png`.png' \;
+cd ..
+
+mkdir image_resized
+cd image_resized
+find ../"$IMAGE_DIR" -name '*.jpg' -exec sh -c 'echo "{}"; convert "{}" -resize 1024x512\> -size 1024x512 xc:black +swap -gravity center -composite `basename "{}" .jpg`.png' \;
+cd ..
