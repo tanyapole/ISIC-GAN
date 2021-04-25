27a14fde27268c45c4432fa0c2caebf933f06f64
nduginets <nduginets@devexperts.com>
2021-04-25 13:11:50 +0300

    logic grouping

--- brasilian-code/resize-images.sh
+++ brasilian-code/resize-images.sh
@@ -36,15 +36,15 @@ echo "image_resized: $image_resized"
 
 mkdir "$attribute_resized"
 cd "$attribute_resized"
-find ${BASE_DIR}/../"$ATTRI_DIR" -name '*.png' -exec sh -c 'echo "{}"; convert "{}" -resize 1024x512\> -size 1024x512 xc:red +swap -gravity center -composite `basename "{}" .png`.png' \;
+find "${BASE_DIR}/$ATTRI_DIR" -name '*.png' -exec sh -c 'echo "{}"; convert "{}" -resize 1024x512\> -size 1024x512 xc:red +swap -gravity center -composite `basename "{}" .png`.png' \;
 cd ..
 
 mkdir "$segmentation_resized"
 cd "$segmentation_resized"
-find ${BASE_DIR}/../"$SEG_DIR" -name '*.png' -exec sh -c 'echo "{}"; convert "{}" -resize 1024x512\> -size 1024x512 xc:red +swap -gravity center -composite `basename "{}" .png`.png' \;
+find "${BASE_DIR}/$SEG_DIR" -name '*.png' -exec sh -c 'echo "{}"; convert "{}" -resize 1024x512\> -size 1024x512 xc:red +swap -gravity center -composite `basename "{}" .png`.png' \;
 cd ..
 
 mkdir "$image_resized"
 cd "$image_resized"
-find ${BASE_DIR}/../"$IMAGE_DIR" -name '*.jpg' -exec sh -c 'echo "{}"; convert "{}" -resize 1024x512\> -size 1024x512 xc:black +swap -gravity center -composite `basename "{}" .jpg`.png' \;
+find ""${BASE_DIR}/$IMAGE_DIR" -name '*.jpg' -exec sh -c 'echo "{}"; convert "{}" -resize 1024x512\> -size 1024x512 xc:black +swap -gravity center -composite `basename "{}" .jpg`.png' \;
 cd ..
