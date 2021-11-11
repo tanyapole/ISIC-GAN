31cfe7e79276c69eb63a74695e1addb866b3abda
nduginets <nduginets@devexperts.com>
2021-11-11 10:18:06 +0300

    rewrite classifier to support multi-label classification

--- isic2018/run_classifier.sh
+++ isic2018/run_classifier.sh
@@ -12,10 +12,10 @@ for split in $SPLITS; do
 
 train_csv="/nfs/home/nduginets/splits/baseline/train_${split}.csv"
 
-CUDA_VISIBLE_DEVICES=$DEVICES python3 train_comet_csv.py with \
+CUDA_VISIBLE_DEVICES=$DEVICES python3 train.py with \
                                 --train_root ${train_root} --train_csv=${train_csv} --epochs=100\
                                 --validate_root=${validate_root} --validate_csv=${validate_csv} --learning_rate 0.001\
                                 --result_dir ${result_dir} --experiment_name "classifier_${split}"
 done
 
-# CUDA_VISIBLE_DEVICES=0 python3 train_comet_csv.py with train_root="/mnt/tank/scratch/nduginets" train_csv="/nfs/home/nduginets/gan-aug-analysis/splits/percentages/1_0/train_0.csv" epochs=100 val_root="/mnt/tank/scratch/nduginets" val_csv="/nfs/home/nduginets/gan-aug-analysis/splits/isic2019-val.csv" model_name="inceptionv4" exp_desc="Real" exp_name="gans.train_Real.inceptionv4.split0"
+# CUDA_VISIBLE_DEVICES=0 python3 train_comet_train_comet_csv.pycsv.py with train_root="/mnt/tank/scratch/nduginets" train_csv="/nfs/home/nduginets/gan-aug-analysis/splits/percentages/1_0/train_0.csv" epochs=100 val_root="/mnt/tank/scratch/nduginets" val_csv="/nfs/home/nduginets/gan-aug-analysis/splits/isic2019-val.csv" model_name="inceptionv4" exp_desc="Real" exp_name="gans.train_Real.inceptionv4.split0"
