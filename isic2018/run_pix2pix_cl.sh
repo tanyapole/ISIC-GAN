17c69d07145e71c49dfbbf3be0e3d8d101105b24
nduginets <nduginets@devexperts.com>
2021-11-11 10:51:25 +0300

    rewrite classifier to support multi-label classification

--- isic2018/run_pix2pix_cl.sh
+++ isic2018/run_pix2pix_cl.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
   
-isic19_root="/mnt/tank/scratch/nduginets"
-
-
-val_csv="/nfs/home/nduginets/gan-aug-analysis/splits/isic2019-val.csv"
-
-train_pix2pix_clf="/nfs/home/nduginets/gan-aug-analysis/splits/different_gans/real-pix2pixhd/train.csv"
+train_root="/mnt/tank/scratch/nduginets"
+validate_root="/mnt/tank/scratch/nduginets"
+result_dir="/mnt/tank/scratch/nduginets/classifiers"
+validate_csv="/nfs/home/nduginets/master-diploma/splits/validation.csv"
 
 DEVICES=$1
 SPLITS=$(echo $2 | tr ";" "\n")
 
 for split in $SPLITS; do
-CUDA_VISIBLE_DEVICES=0 python3 train_comet_csv.py with \
-                                train_root=${isic19_root} train_csv=${train_pix2pix_clf} epochs=100\
-                                val_root=${isic19_root} val_csv=${val_csv} model_name="inceptionv4" exp_desc="pix2pix"\
-                                exp_name="gans.train_pix2pix.inceptionv4.split${split}"
+
+train_csv="/nfs/home/nduginets/master-diploma/splits/generated/train_${split}.csv"
+
+CUDA_VISIBLE_DEVICES=$DEVICES python3 train.py \
+                                --train_root ${train_root} --train_csv=${train_csv} --epochs=100\
+                                --validate_root=${validate_root} --validate_csv=${validate_csv} --learning_rate 0.001\
+                                --result_dir ${result_dir} --experiment_name "classifier_${split}"
 done
+
 
 #CUDA_VISIBLE_DEVICES=0 python3 train_comet_csv.py with train_root="/mnt/tank/scratch/nduginets"\
 #  train_csv="/nfs/home/nduginets/gan-aug-analysis/splits/different_gans/real-pix2pixhd/train.csv"\
