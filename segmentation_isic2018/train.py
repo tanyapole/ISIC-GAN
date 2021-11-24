5a8ce68c9ad7692680f131bdc5697d2fa735a82c
nduginets <nduginets@devexperts.com>
2021-11-25 01:00:19 +0300

    segmentations

--- segmentation_isic2018/train.py
+++ segmentation_isic2018/train.py
@@ -129,10 +129,10 @@ def train_epoch(device, model, dataloaders, metric_holder, criterion, optimizer,
         losses.update(loss.item(), inputs.size(0))
         accuracies.update(torch.sum(output_copy == labels).item(),
                           (output_copy.shape[0] * output_copy.shape[1] * output_copy.shape[2] * output_copy.shape[3]))
-        iou_metric.update(
-            iou_function(torch.tensor(output_copy, dtype=torch.int).to(device),
-                         torch.tensor(labels, dtype=torch.int).to(device)).item(),
-            output_copy.shape[0])
+
+        iou_output = torch.tensor(output_copy, dtype=torch.int).to(device)
+        iou_label = torch.tensor(labels, dtype=torch.int).to(device)
+        iou_metric.update(iou_function(iou_output, iou_label).item(), output_copy.shape[0])
 
         tqdm_loader.set_postfix(loss=losses.avg, iou=iou_metric.avg, acc=accuracies.avg, epoch=epoch_number)
 
@@ -142,8 +142,7 @@ def train_epoch(device, model, dataloaders, metric_holder, criterion, optimizer,
             result_cell[label_name]['avg'].update(cnt, count)
 
             result_cell[label_name]['iou'].update(
-                iou_function(torch.tensor(output_copy[:, idx, :, :], dtype=torch.int).to(device),
-                             torch.tensor(labels[:, idx, :, :], dtype=torch.int).to(device)).item(), output_copy.shape[0])
+                iou_function(iou_output[:, idx, :, :], iou_label[:, idx, :, :]).item(), output_copy.shape[0])
 
     for label_name in label_names:
         result_cell[label_name]['avg'] = result_cell[label_name]['avg'].avg
