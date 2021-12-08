af89cff9e0f4f8a80c7b494eea7e84c8df740af6
nduginets <nduginets@devexperts.com>
2021-12-08 11:35:19 +0300

    segmentatino improvements

--- segmentation_isic2018/train.py
+++ segmentation_isic2018/train.py
@@ -32,11 +32,12 @@ SMOOTH = 1e-6
 
 
 def iou_pytorch(outputs: torch.Tensor, labels: torch.Tensor):
-    intersection = (outputs & labels).float().sum((2, 3))  # Will be zero if Truth=0 or Prediction=0
-    union = (outputs | labels).float().sum((2, 3))  # Will be zzero if both are 0
-    iou = (intersection + SMOOTH) / (union + SMOOTH)  # We smooth our devision to avoid 0/0
-    thresholded = torch.clamp(20 * (iou - 0.5), 0, 10).ceil() / 10  # This is equal to comparing with thresolds
-    return thresholded.mean().item()  # Or thresholded.mean() if you are interested in average across the batch
+    intersection = (outputs & labels).float().sum((1, 2, 3))  # Will be zero if Truth=0 or Prediction=0
+    union = (outputs | labels).float().sum((1, 2, 3))  # Will be zzero if both are 0
+    # iou = (intersection + SMOOTH) / (union + SMOOTH)  # We smooth our devision to avoid 0/0
+    # thresholded = torch.clamp(20 * (iou - 0.5), 0, 10).ceil() / 10  # This is equal to comparing with thresolds
+    # return thresholded.mean().item()  # Or thresholded.mean() if you are interested in average across the batch
+    return intersection.to("cpu"), union.to("cpu")
 
 
 def iou_pytorch_by_class(idx, outputs: torch.Tensor, labels: torch.Tensor):
@@ -44,6 +45,15 @@ def iou_pytorch_by_class(idx, outputs: torch.Tensor, labels: torch.Tensor):
     labels = labels[:, idx, :, :]
     intersection = (outputs & labels).float().sum((1, 2))  # Will be zero if Truth=0 or Prediction=0
     union = (outputs | labels).float().sum((1, 2))  # Will be zzero if both are 0
+    # iou = (intersection + SMOOTH) / (union + SMOOTH)  # We smooth our devision to avoid 0/0
+    # thresholded = torch.clamp(20 * (iou - 0.5), 0, 10).ceil() / 10  # This is equal to comparing with thresolds
+    # return thresholded.mean().item()  # Or thresholded.mean() if you are interested in average across the batch
+    return intersection.to("cpu"), union.to("cpu")
+
+
+def calculate_iou(list_intersections, list_unions):
+    intersection = torch.cat(list_intersections)
+    union = torch.cat(list_unions)
     iou = (intersection + SMOOTH) / (union + SMOOTH)  # We smooth our devision to avoid 0/0
     thresholded = torch.clamp(20 * (iou - 0.5), 0, 10).ceil() / 10  # This is equal to comparing with thresolds
     return thresholded.mean().item()  # Or thresholded.mean() if you are interested in average across the batch
@@ -97,7 +107,9 @@ def train_epoch(device, model, dataloaders, metric_holder, criterion, optimizer,
                 mask_saver):
     losses = AverageMeter()
     accuracies = AverageMeter()
-    result_cell = {i: {} for i in label_names}
+    result_iou = {i: ([], []) for i in label_names}
+    base_iou = ([], [])
+
     if phase == 'train':
         model.train()
         meters = metric_holder['train']
@@ -114,9 +126,9 @@ def train_epoch(device, model, dataloaders, metric_holder, criterion, optimizer,
 
     # int_labels_cat = None
     # output_cat = None
-    idx = 0
+    idx_ = 0
     for data in tqdm_loader:
-        idx += 1
+        idx_ += 1
         (inputs, labels, int_labels), name = data
 
         inputs = inputs.to(device)
@@ -143,17 +155,23 @@ def train_epoch(device, model, dataloaders, metric_holder, criterion, optimizer,
         accuracies.update(torch.sum(output_copy == labels).item(),
                           (output_copy.shape[0] * output_copy.shape[1] * output_copy.shape[2] * output_copy.shape[3]))
 
-        mask_saver.write_masks(idx, int_labels, output_copy)
-        # int_labels_cat = concat(int_labels_cat, int_labels)
-        # output_cat = concat(output_cat, output_copy)
-
+        # mask_saver.write_masks(idx, int_labels, output_copy)
+        intersection, union = iou_pytorch(output_copy, int_labels)
+        base_iou[0].append(intersection), base_iou[1].append(union)
+        for idx, i in enumerate(label_names):
+            intersection, union = iou_pytorch_by_class(idx, output_copy, int_labels)
+            result_iou[i][0].append(intersection), result_iou[i][1].append(union)
         tqdm_loader.set_postfix(loss=losses.avg, acc=accuracies.avg, epoch=epoch_number)
 
-    mask_saver.end()
-    result_cell['loss'] = losses.avg
-    result_cell['accuracy'] = accuracies.avg
+    # mask_saver.end()
+    result_iou['loss'] = losses.avg
+    result_iou['accuracy'] = accuracies.avg
+    result_iou['iou'] = calculate_iou(base_iou[0], base_iou[1])
+    for idx, i in enumerate(label_names):
+        intersection, union = result_iou[i]
+        result_iou[i] = calculate_iou(intersection, union)
 
-    meters.add_record(epoch_number, result_cell)
+    meters.add_record(epoch_number, result_iou)
 
 
 def save_images(dataset, to, n=32):
@@ -263,22 +281,22 @@ def main(train_root, train_csv, val_root, val_csv, epochs: int, batch_size: int,
 
     for epoch in epochs_list:
         logging.debug('train epoch {}/{}'.format(epoch + 1, epochs))
-        train_mask_saver = MasksSaver(experiment_path, "train_{}".format(epoch))
+        # train_mask_saver = MasksSaver(experiment_path, "train_{}".format(epoch))
         train_epoch(
             device, model, dataloaders, metric_holder, criterion, optimizer, 'train',
             epoch, epochs,
             labels,
-            batches_per_epoch, train_mask_saver)
+            batches_per_epoch, None)
 
         if val_root is not None:
-            test_mask_saver = MasksSaver(experiment_path, "test_{}".format(epoch))
+            # test_mask_saver = MasksSaver(experiment_path, "test_{}".format(epoch))
 
             logging.debug('val epoch {}/{}'.format(epoch + 1, epochs))
             train_epoch(
                 device, model, dataloaders, metric_holder, criterion, optimizer, 'val',
                 epoch, epochs,
                 labels,
-                batches_per_epoch, test_mask_saver)
+                batches_per_epoch, None)
             logging.debug('-' * 40)
 
         scheduler.step()
@@ -299,7 +317,7 @@ if __name__ == "__main__":
             "--result_dir", "/Users/nduginets/Desktop",
             "--experiment_name", "tmp",
             "--num_workers", "0",  # stupid Mac os!!!!
-            "--batch_size", "7"
+            "--batch_size", "2"
         ])
     else:
         params = pl.initialize()
