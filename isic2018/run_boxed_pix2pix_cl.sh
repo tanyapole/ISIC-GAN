6e3eed80730ec23b2f43c53a35494b4fdf14e649
nduginets <nduginets@devexperts.com>
2022-01-16 20:07:02 +0300

    bounding box

---
+++ isic2018/run_boxed_pix2pix_cl.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+
+if [ -z "${VALIDATION}" ]
+then
+  echo "required VALIDATION argument"
+  exit 1
+fi
+echo "$VALIDATION"
+echo ""
+
+if [ "$VALIDATION" != "validation_skin_lesion" ] && [ "$VALIDATION" != "validation" ]
+then
+  echo "must be validation_skin_lesion or validation"
+  exit 1
+fi
+
+train_root="/mnt/tank/scratch/nduginets"
+validate_root="/mnt/tank/scratch/nduginets"
+result_dir="/mnt/tank/scratch/nduginets/classifiers"
+validate_csv="/nfs/home/nduginets/master-diploma/splits_boxed/$VALIDATION.csv"
+
+if [ "$VALIDATION" == "validation_skin_lesion" ]
+then
+  GENERATED_PATH="generated_bussio"
+  EXTENSION="bussio_"
+fi
+
+if [ "$VALIDATION" == "validation" ]
+then
+  GENERATED_PATH="generated"
+  EXTENSION=""
+fi
+
+DEVICES=$1
+EXTENDED=$2
+SPLITS=$(echo $3 | tr ";" "\n")
+
+for split in $SPLITS; do
+train_csv="/nfs/home/nduginets/master-diploma/splits/$GENERATED_PATH/train_${EXTENDED}_${split}.csv"
+CUDA_VISIBLE_DEVICES=$DEVICES python3 train.py \
+                                --train_root ${train_root} --train_csv=${train_csv} --epochs=100\
+                                --validate_root=${validate_root} --validate_csv=${validate_csv} --learning_rate 0.001\
+                                --result_dir ${result_dir} --experiment_name "${EXTENSION}boxed_pix_2pix_classifier_${EXTENDED}_${split}"
+done
+
+
+
+#CUDA_VISIBLE_DEVICES=0 python3 train_comet_csv.py with train_root="/mnt/tank/scratch/nduginets"\
+#  train_csv="/nfs/home/nduginets/gan-aug-analysis/splits/different_gans/real-pix2pixhd/train.csv"\
+#  epochs=100 val_root="/mnt/tank/scratch/nduginets" val_csv="/nfs/home/nduginets/gan-aug-analysis/splits/isic2019-val.csv"\
+#  model_name="inceptionv4" exp_desc="pix2pix" exp_name="gans.train_pix2pix.inceptionv4.split0"
