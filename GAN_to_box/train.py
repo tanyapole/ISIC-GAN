6a24aba74914657b3dc59ba1c12d2077f6252955
nduginets <nduginets@devexperts.com>
2022-03-02 23:11:29 +0300

    gan

--- GAN_to_box/train.py
+++ GAN_to_box/train.py
@@ -192,9 +192,9 @@ def train_epoch(device,
                                 acc=("D=" + str(accuracies["D"].avg), "G=" + str(accuracies["true_G"].avg)),
                                 _epoch=epoch_number)
         if idx == 0:
-            print("fake_d_output", fake_d_output[0: 100])
-            print("real_d_output", real_d_output[0: 100])
-            print("fake_g_output", fake_g_output[0: 100])
+            print("fake_d_output", fake_d_output.T[0: 100])
+            print("real_d_output", real_d_output.T[0: 100])
+            print("fake_g_output", fake_g_output.T[0: 100])
 
 
     result_cell = {}
